bdcb168f7e88aec9a6e31f7be5ba50f069f23457
Ryna Truong <rynatruong@outlook.com>
2020-06-23 15:27:30 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,10 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length 
-  yield(collection[i].find.even?)
+  yield(collection[i])
     i += 1
+    if collection[i].even?
+      collection[i]
   end
   collection[i]
 end
